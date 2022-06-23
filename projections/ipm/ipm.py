a5ef602d5055355c6fdb5f6f34d20f29dba99221
Sanjay Garg <55763713+laser-vis@users.noreply.github.com>
2022-06-23 09:06:28 -0400

    Formatted by black

--- projections/ipm/ipm.py
+++ projections/ipm/ipm.py
@@ -4,7 +4,9 @@ import numpy as np
 
 from utils import perspective, Plane, load_camera_params, bilinear_sampler, warped
 
-image = cv2.cvtColor(cv2.imread('stuttgart_01_000000_003715_leftImg8bit.png'), cv2.COLOR_BGR2RGB)
+image = cv2.cvtColor(
+    cv2.imread("stuttgart_01_000000_003715_leftImg8bit.png"), cv2.COLOR_BGR2RGB
+)
 interpolation_fn = bilinear_sampler  # or warped
 TARGET_H, TARGET_W = 500, 800
 
@@ -20,25 +22,34 @@ def ipm_from_opencv(image, source_points, target_points):
     # Compute projection matrix
     M = cv2.getPerspectiveTransform(source_points, target_points)
     # Warp the image
-    warped = cv2.warpPerspective(image, M, (TARGET_W, TARGET_H), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT,
-                                 borderValue=0)
+    warped = cv2.warpPerspective(
+        image,
+        M,
+        (TARGET_W, TARGET_H),
+        flags=cv2.INTER_LINEAR,
+        borderMode=cv2.BORDER_CONSTANT,
+        borderValue=0,
+    )
     return warped
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ################
     # Derived method
     ################
     # Define the plane on the region of interest (road)
     plane = Plane(0, -25, 0, 0, 0, 0, TARGET_W, TARGET_H, 0.1)
     # Retrieve camera parameters
-    extrinsic, intrinsic = load_camera_params('camera.json')
+    extrinsic, intrinsic = load_camera_params("camera.json")
     # Apply perspective transformation
-    warped1 = ipm_from_parameters(image, plane.xyz, intrinsic, extrinsic, interpolation_fn)
+    warped1 = ipm_from_parameters(
+        image, plane.xyz, intrinsic, extrinsic, interpolation_fn
+    )
 
     ################
     # OpenCV
     ################
+    # fmt: off
     # Vertices coordinates in the source image
     s = np.array([[830, 598],
                   [868, 568],
@@ -50,6 +61,7 @@ if __name__ == '__main__':
                   [213, 231],
                   [178, 264],
                   [216, 264]], dtype=np.float32)
+    # fmt: on
 
     # Warp the image
     warped2 = ipm_from_opencv(image, s, t)
@@ -58,10 +70,10 @@ if __name__ == '__main__':
     fig, ax = plt.subplots(1, 3)
 
     ax[0].imshow(image)
-    ax[0].set_title('Front View')
+    ax[0].set_title("Front View")
     ax[1].imshow(warped1)
-    ax[1].set_title('IPM')
+    ax[1].set_title("IPM")
     ax[2].imshow(warped2)
-    ax[2].set_title('IPM from OpenCv')
+    ax[2].set_title("IPM from OpenCv")
     plt.tight_layout()
     plt.show()
